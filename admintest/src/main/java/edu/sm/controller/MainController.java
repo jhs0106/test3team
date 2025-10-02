8c55848a0b7e267ab4462fb7843aaba1655862fa
jhs0106 <joohsd87@naver.com>
2025-10-02 16:31:33 +0900

    1002

---
+++ admintest/src/main/java/edu/sm/controller/MainController.java
@@ -0,0 +1,21 @@
+package edu.sm.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@Slf4j
+public class MainController {
+
+    @RequestMapping("/")
+    public String main(Model model) {
+        model.addAttribute("center", "center");
+        model.addAttribute("left", "left");
+        return "index";
+    }
+
+}
---
+++ usertest/src/main/java/edu/sm/app/dto/dto.java
@@ -0,0 +1,13 @@
+package edu.sm.app.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Builder
+public class dto {
+}
+//폴더 생성을 위한 예시 파일입니다
