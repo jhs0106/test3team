79ed312f68d901882de1e5b61608a2375100ba40
jhs0106 <joohsd87@naver.com>
2025-10-10 11:32:20 +0900

    Add files via upload

---
+++ admintest/src/main/java/edu/sm/app/repository/repository.java
@@ -0,0 +1,5 @@
+package edu.sm.app.repository;
+
+public interface repository {
+}
+//폴더 생성을 위한 예시 파일입니다//
---
+++ admintest/src/main/java/edu/sm/app/service/service.java
@@ -0,0 +1,6 @@
+package edu.sm.app.service;
+
+public class service {
+}
+
+//폴더 생성을 위한 예시 파일입니다//
